b36ee0c6cd9261a85c5cdbd3181f7f800ea82cf3
Federico Sordillo <federicosordillo@gmail.com>
2017-05-26 16:38:42 +0200

    still no username but almost

---
+++ src/scratch/userapi.clj
@@ -0,0 +1,14 @@
+(ns scratch.userapi
+  (:require [scratch.core :refer [getCall]]
+            [clojure.data.json :as json]))
+
+
+(defn resolve-users [context args _value]
+  (json/read-str (:body (getCall "http://private-339a33-user176.apiary-mock.com/users"))))
+
+
+(defn resolve-user []
+  (list))
+
+(defn resolve-create-user! []
+  (list))
